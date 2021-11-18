fc2b3a69a241050eba3720b2ebba71bb7088ae77
Matt Holmes <m4ttholmes@gmail.com>
2021-11-18 14:51:09 -0500

    Adventure Table full CRUD functionality - update and delete now implemented

--- src/components/Adventures/Adventure.tsx
+++ src/components/Adventures/Adventure.tsx
@@ -1,23 +1,49 @@
 import React, { Component } from "react"
-import CreateCharacter from "./CreateAdventure"
+import CreateAdventure from "./CreateAdventure"
 import DisplayAdventure from "./DisplayAdventure"
+import UpdateAdventure from "./UpdateAdventure"
 
 type AdventureState = {
     adventures: []
     createActive: boolean
+    updateActive: boolean
+    updatedAdventure: AdventureDetails
 }
-
 
 type AuthProps = {
     sessionToken: string | undefined | null
 }
+
+
+type AdventureDetails = {
+    actType: string
+    advName: string
+    date: string
+    location: string
+    rating: number
+    thoughts: string
+    private: boolean
+    id: string
+}
+
 
 export default class Adventure extends Component<AuthProps, AdventureState> {
     constructor(props: AuthProps) {
         super(props)
         this.state = {
             adventures: [],
-            createActive: false
+            createActive: false,
+            updateActive: false,
+            updatedAdventure: {
+                actType: "",
+                advName: "",
+                date: "",
+                location: "",
+                rating: 0,
+                thoughts: "",
+                private: true,
+                id: ""
+            }
         }
     }
 
@@ -32,6 +58,20 @@ export default class Adventure extends Component<AuthProps, AdventureState> {
     createOff = () => {
         this.setState({createActive: false})
     }
+    
+    updateOn = () => {
+        this.setState({updateActive: true})
+    }
+    
+    updateOff = () => {
+        this.setState({updateActive: false})
+    }
+    
+    editUpdateAdventure = (adventureDetails: AdventureDetails) => {
+        this.setState({updatedAdventure: adventureDetails})
+    }
+
+
 
     fetchMyAdventures = () => {
         console.log("FetchMyAdventures Function Called");
@@ -45,7 +85,7 @@ export default class Adventure extends Component<AuthProps, AdventureState> {
         .then(response => response.json())
         .then(data => {
             this.setState({
-                adventures: data.userAdventures
+                adventures: data.userAdventures.reverse()
             })
             console.log(this.state.adventures);
         })
@@ -54,12 +94,39 @@ export default class Adventure extends Component<AuthProps, AdventureState> {
 
     }
 
+    deleteAdventure = (advId: string) => {
+        const proceed = window.confirm("Are you certain you wish to delete this adventure? This is a permanent action that cannot be undone.");
+        
+        if (proceed) {
+            console.log("DeleteAdventure Function Called");
+            console.log(advId);
+
+            fetch(`http://localhost:5000/adventure/${advId}`, {
+                method: "DELETE",
+                headers: new Headers({
+                    "Content-Type": "application/json",
+                    "Authorization": `${this.props.sessionToken}`
+                })
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                this.fetchMyAdventures();
+            })
+            .catch(err => console.log(err))
+
+        } else {
+            console.log("Delete cancelled.");
+        }
+    }
+
 
     render(){
         return(
             <div>
-                {this.state.createActive ? <CreateCharacter createOff={this.createOff} fetchMyAdventures={this.fetchMyAdventures} sessionToken={this.props.sessionToken}/> : null }
-                <DisplayAdventure createOn={this.createOn} adventures={this.state.adventures} fetchMyAdventures={this.fetchMyAdventures} sessionToken={this.props.sessionToken}/>
+                {this.state.updateActive ? <UpdateAdventure updatedAdventure={this.state.updatedAdventure} updateOff={this.updateOff} fetchMyAdventures={this.fetchMyAdventures} sessionToken={this.props.sessionToken}/> : null }
+                {this.state.createActive ? <CreateAdventure createOff={this.createOff} fetchMyAdventures={this.fetchMyAdventures} sessionToken={this.props.sessionToken}/> : null }
+                <DisplayAdventure deleteAdventure={this.deleteAdventure} updateOn={this.updateOn} editUpdateAdventure={this.editUpdateAdventure} createOn={this.createOn} adventures={this.state.adventures} fetchMyAdventures={this.fetchMyAdventures} sessionToken={this.props.sessionToken}/>
             </div>
         )
     }
